0b692ae8365751b4680eeb1d3b50c12ff6b26a3a
Realittle <1609344199@qq.com>
2024-10-10 12:38:26 +0800

    Edit Video Script Online

--- src/examples/hello-movy.js
+++ src/examples/hello-movy.js
@@ -1,7 +1,16 @@
 import * as mo from 'movy';
+// npm run dev 即可在editor实时同步
+// mo.addText({})
 
-mo.addText('Hello, Movy!', {
-  scale: 1,
-  color: 'yellow',
-  font: 'en',
-}).reveal();
+mo.addGrid({ gridSize: 100}); // 用于
+
+mo.addCircleOutline(
+  {x:-3.5,y:0,radius:1.5,
+    lineWidth:0.3,color: "#3498db"}
+).reveal()
+mo.addCircleOutline({x:3.5,y:0,radius:1.5}).reveal()
+
+mo.addArrow(
+  [-1.7, 0], [2, 0], 
+  { lineWidth: 0.07 }
+).drawLine({ duration: 1.5 });
